a1ecd69173199214d8d5afb75746631b46d4c54c
lcylichenyi <289172988@qq.com>
2019-02-05 00:21:18 +0800

    u

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 //默认打包的入口是.src/index.js 所以名字不要错
 
-console.log('woca')
+console.log('woaaaca')
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+const htmlPlugin = new HtmlWebPackPlugin({
+	template: path.join(__dirname,'./src/index.html'), //源文件
+	filename: 'index.html'
+})
+
 //向外暴露一个打包的配置对象
 module.exports = {
 	//如果想压缩的话用production，开发的时候便于阅读就用development
-	mode: 'development'
-
+	mode: 'development',
 	//webpack 4.x,默认的打包入口路径是src->index.js
+	plugins: [
+	htmlPlugin
+	]
 }
